b949256dbaf63cf0328082da33b1d372169044e1
foodstuffsConsumer <160200985+foodstuffsConsumer@users.noreply.github.com>
2025-03-19 10:10:12 +1100

    added actual code into my functions
    still need to fix requests not being resolved

--- my_module.py
+++ my_module.py
@@ -1,26 +1,57 @@
 import time
 
+import requests
+
+print(requests.__file__)
+
+print("done")
+
+API_URL = "https://www.dnd5eAPI_URL.co/API_URL/spells/"
+
 spellbook = {}
 
-def searcher():
-    print("doesn't work yet")
-    print('')
+def searcher(spell):
+    response = requests.get(API_URL + spell.lower().replace(" ", "-"))
+    if response.status_code == 200:
+        spell_data = response.json()
+        return {
+            "name": spell_data["name"],
+            "level": spell_data["level"],
+            "description": spell_data["desc"][0]
+        }
+    else:
+        print("Spell not found.")
+        return None
+print('')
 
-def adder():
-    print("not yet buddy pal")
-    print('')
+def adder(spell):
+    spellname = searcher(spell)
+    if spellname:
+        spellbook[spell] = spellname
+        print(f"Added {spell} to your spellbook!")
+print('')
 
 def viewer():
-    print("under construction")
-    print('')
+    if not spellbook:
+        print("Your spellbook is empty.")
+    else:
+        for spell in spellbook.values():
+            print(f"{spell['name']} (Level {spell['level']}): {spell['description']}")
+print('')
 
-def remover():
-    print("give time where time is due") 
-    # did you know? the printed statement above is a reference. if you know what it is, you're based. based on what? idk man
-    print('')
+def remover(spell):
+    if not spellbook:
+        print("Your spellbook is empty.")
+    else:
+        if spellbook[spell]:
+            spellbook.pop(spell)
+            print(f"Removed {spell} from your spellbook.")
+        else:
+            print("Spell not found.")
+print('')
 
 def invalid():
-    print("dude this wouldn't be valid even if the program worked")
+    print("Please type in a valid command.")
     print('')
 
 def main():
@@ -39,7 +70,7 @@ def main():
         choice = rawchoice.lower()
 
         if choice == 'search':
-            searcher()
+            searcher(choice)
         elif choice == 'add':
             adder()
         elif choice == 'view':
